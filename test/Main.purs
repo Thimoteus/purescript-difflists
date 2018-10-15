bc144e878d79d11e6eff51c4f71258cae21582ec
parenparen <dtmuelle@gmail.com>
2018-10-14 21:41:57 -0700

    add test logging

--- test/Main.purs
+++ test/Main.purs
@@ -2,6 +2,7 @@ module Test.Main where
 
 import Prelude
 import Effect (Effect)
+import Effect.Console (log)
 import Test.DList.Alternative as Alternative
 import Test.DList.Functor as Functor
 import Test.DList.MonadPlus as MonadPlus
@@ -13,11 +14,19 @@ import Test.DList.Ord as Ord
 
 main :: Effect Unit
 main = do
+  log "Running alternative tests ..."
   Alternative.spec
+  log "Running functor tests ..."
   Functor.spec
+  log "Running monadplus tests ..."
   MonadPlus.spec
+  log "Running monoid tests ..."
   Monoid.spec
+  log "Running applicative tests ..."
   Applicative.spec
+  log "Running isomorphism tests ..."
   Isomorphism.spec
+  log "Running monad tests ..."
   Monad.spec
+  log "Running partial order tests ..."
   Ord.spec
